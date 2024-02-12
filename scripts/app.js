1178f309d45f35e00070f36e5eaa04c800e73820
Chsser <chsebarry80@gmail.com>
2024-02-12 18:01:09 -0500

    Page Content Commit
    Co-authored-by: YungKnighty <YungKnighty@users.noreply.github.com>

--- scripts/app.js
+++ scripts/app.js
@@ -1,44 +1,21 @@
-/*
-change the class of each box, reorder the colours
-get the boxes with jquery
-loop through each box
-get the class of the current box
-if the class is red make it blue
-if the class is blue make it green
-if the class is green make it red
-create a button to open the modal
-get the button with jquery 
-add a click event listener to the button
-when the button is clicked, show the modal
-*/
+// import ‘../node_modules/<package name>/<css file to import>’;
 
-
-let boxes = $('.box');
-
-
-boxes.each(function(index, box){
-
-    let currentClass = $(box).attr('class');
-
-    if (currentClass === 'box red-box'){
-        $(box).attr('class', 'blue-box');
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
     }
-
-    else if (currentClass === 'box blue-box'){
-        $(box).attr('class', 'green-box');
-    }
-
-    else if (currentClass === 'box green-box'){
-        $(box).attr('class', 'red-box');
-    }
-
-});
-
-
-let button = $('<button>Open Modal</button>');
-
-$('#content').append(button);
-
-button.on('click', function(){
-    $('.lightbox').show();
-});
+  }
